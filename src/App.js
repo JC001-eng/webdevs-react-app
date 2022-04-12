654e4689e5900833925747b6e71edc5477fba624
Justin Cefai <justin@ip-10-78-0-14.ec2.internal>
2022-04-12 11:55:25 -0700

    counter added with state

--- src/App.js
+++ src/App.js
@@ -1,14 +1,16 @@
-import logo from "./logo.svg";
 import "./App.css";
 import Header from "./Header";
 import Button from "./Button";
+import Counter from "./Counter"
 import { Link } from "react-router-dom";
+
 
 function App() {
 
   return (
     <div className="App font-main">
       <Header name="Sara" title="Page 1" />
+      <Counter />
       <Link to="/page2">
         <Button />
       </Link>
